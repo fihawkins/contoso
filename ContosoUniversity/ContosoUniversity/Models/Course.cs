f768519f081cd8c4aaf6a670ffcc556d7ff018f7
fihawkins <filipe.ufabc@gmail.com>
2017-01-26 22:35:47 -0200

    proximo p 22 create the database context

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        //Declaro que não quero que o banco gere a PK para mim.
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
