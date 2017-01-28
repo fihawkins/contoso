18418cf848e9c35ad4480581b8df747e3e4ded37
filipeengage <filipe.lopes@engage.bz>
2017-01-28 12:41:51 -0200

    next p26 Creating a Student Controller and Views

---
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        //entity set = database table ~= dbset?
+        //entity = table row
+
+        //ConnectionString name being passed through constructor
+        //If didn't pass, would look for a connstring with this class name
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
--- ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -5,22 +5,19 @@ using System.Web;
 
 namespace ContosoUniversity.Models
 {
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
     public class Enrollment
     {
-        public enum Grade
-        {
-            A, B, C, D, F
-        }
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
 
-        public class Enrollment
-        {
-            public int EnrollmentID { get; set; }
-            public int CourseID { get; set; }
-            public int StudentID { get; set; }
-            public Grade? Grade { get; set; }
-
-            public virtual Course Course { get; set; }
-            public virtual Student Student { get; set; }
-        }
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
     }
 }
