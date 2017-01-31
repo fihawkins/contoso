7bb259508efe5d2fdd70eec4c3ead6fdbef2a945
filipeengage <filipe.lopes@engage.bz>
2017-01-31 17:04:16 -0200

    data annotations in student model

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
@@ -7,7 +8,19 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
         public string LastName { get; set; }
+
+        //[Required]
+        [Required(ErrorMessage = "Oh my gosh! Please enter the student's first name.")]
+        [StringLength(50)]
+        //[StringLength(50, ErrorMessage = "The first name must be less than 50 characters")]
+        [Display(Name ="First Name:")]
         public string FirstMidName { get; set; }
+
+        //ModelState consider valid yyyy-mm-dd, is it possible to change it?
+        //this annotation adds a datepicker to the html field of this property
+        [DataType(DataType.Date)]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        //[Display(Name = "Enrollment Date:")]
         public DateTime EnrollmentDate { get; set; }
         public string EmailAddress { get; set; }
 
