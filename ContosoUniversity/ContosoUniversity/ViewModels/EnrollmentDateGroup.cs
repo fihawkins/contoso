b29c5a793da2d2d493febb4d41fdac8869ec05d0
fihawkins <filipe.ufabc@gmail.com>
2017-03-11 11:24:30 -0300

    next p76 connection resiliency

---
+++ ContosoUniversity/ContosoUniversity/ViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.ViewModels
+{
+    //viewmodels are used to pass specific data of a model to the view, 
+    //without having to pass all the properties, such as password.
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+        public int StudentCount { get; set; }
+    }
+}
