2d2586c44da65d04cac4ab71aa073c6a50073517
filipeengage <filipe.lopes@engage.bz>
2017-03-22 12:33:48 -0300

    next p87 Test logging

--- ContosoUniversity/ContosoUniversity/DAL/SchoolInitializer.cs
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolInitializer.cs
@@ -1,8 +1,6 @@
 ﻿using ContosoUniversity.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace ContosoUniversity.DAL
 {
--- ContosoUniversity/ContosoUniversity/Global.asax.cs
+++ ContosoUniversity/ContosoUniversity/Global.asax.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ContosoUniversity.DAL;
+using System.Data.Entity.Infrastructure.Interception;
+using ContosoUniversity.Logging;
 
 namespace ContosoUniversity
 {
@@ -16,6 +15,8 @@ namespace ContosoUniversity
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
         }
     }
 }
